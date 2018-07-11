2f7bdda32ddbee296446dfe5a5dc0933a94f3c11
Jordan Harris <jharris@wework.com>
2018-07-11 01:15:53 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,12 +28,17 @@ def monopoly_with_third_tier
 			 monopoly = {
    :railroads => {
       :pieces => 4,
-      :names => {},
+      :names => {
+        :one_piece_owned => 25,
+        :two_piece_owned => 50,  
+        :three_piece_owned => 100,  
+        :four_pieces_owned => 200
+      },
       :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_piece_owned => 50,  
         :three_piece_owned => 100,  
-
+        :four_pieces_owned => 200
       }
       }
    }
